6e994cd4cd2eafc92362349a70865760beb5d4ad
Kelly Sandim <kelly.iwauchi@gmail.com>
2020-06-02 12:15:58 -0400

    Update code with content of class #4

--- main.js
+++ main.js
@@ -7,6 +7,17 @@ var app = new Vue({
         link: 'https://www.marisa.com.br/masculino/c/meias-m', // parte do challenge #2
         //inventory: 0
         inStock: false,
-        onSale: true // parte do challenge #3
+        onSale: true, // parte do challenge #3
+        details:  ["80% algodão", "70% poliéster", "Gênero-neutro"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "verde"
+            },
+            {
+                variantId: 2235,
+                variantColor: "azul"
+            }
+        ]
     }
 })
