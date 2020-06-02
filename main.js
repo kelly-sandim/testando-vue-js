1ed75e3b891d1c8fc799b555db37ac99e2be11f5
Kelly Sandim <kelly.iwauchi@gmail.com>
2020-06-02 10:30:43 -0400

    Doing the vue.js second challenge

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 var app = new Vue({
     el: '#app',
     data: {
-        product: 'Meias',
-        // parte do challenge
-        description: 'Par de meias fedidas',
-        image: './assets/vmSocks-green.jpg' 
+        product: 'Meias',        
+        description: 'Par de meias fedidas', // parte do challenge #1
+        image: './assets/vmSocks-green.jpg',
+        link: 'https://www.marisa.com.br/masculino/c/meias-m' // parte do challenge #2
     }
 })
