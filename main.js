4039ad24ed7f621042ca603862c7d8134243ebd7
Kelly Sandim <kelly.iwauchi@gmail.com>
2020-06-04 16:46:04 -0400

    Update code with content of class #6

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ var app = new Vue({
     el: '#app',
     data: {
         product: 'Meias',        
-        description: 'Par de meias fedidas', // parte do challenge #1
+        description: 'Par de meias velhas', // parte do challenge #1
         image: './assets/vmSocks-green.jpg',
-        link: 'https://www.marisa.com.br/masculino/c/meias-m', // parte do challenge #2
+        link: 'https://www.google.com/search?q=meias&tbm=isch&ved=2ahUKEwiquY2PtujpAhUiALkGHT7-CDoQ2-cCegQIABAA&oq=meias&gs_lcp=CgNpbWcQAzIECCMQJzICCAAyAggAMgQIABBDMgQIABBDMgIIADICCAAyAggAMgIIADICCABQ6hdY5xhgnxpoAHAAeACAAYoBiAGOApIBAzAuMpgBAKABAaoBC2d3cy13aXotaW1n&sclient=img&ei=KgzZXqr8HaKA5OUPvvyj0AM&bih=657&biw=1366', // parte do challenge #2
         //inventory: 0
         inStock: false,
         onSale: true, // parte do challenge #3
@@ -12,12 +12,12 @@ var app = new Vue({
         variants: [
             {
                 variantId: 2234,
-                variantColor: 'verde',
+                variantColor: 'green',
                 variantImage: './assets/vmSocks-green.jpg'
             },
             {
                 variantId: 2235,
-                variantColor: 'azul',
+                variantColor: 'blue',
                 variantImage: './assets/vmSocks-blue.jpg'
             }
         ],
